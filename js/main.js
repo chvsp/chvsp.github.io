6f9b4265a72edd7e21982eb863dc3d67134da35b
Praveen Ch <chvsp972911@gmail.com>
2017-03-08 05:37:57 +1100

    separated classes

--- js/main.js
+++ js/main.js
@@ -12,6 +12,16 @@ $(document).ready(function () {
       $('.panel-cover').css('max-width', currentWidth)
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
+  $('a.about-button').click(function (e) {
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInLeft')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
